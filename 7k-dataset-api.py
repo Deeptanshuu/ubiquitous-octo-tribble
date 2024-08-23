27e555aa2045de068a47409946f28f42292bfac3
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2024-08-23 19:25:41 +0530

    test

--- 7k-dataset-api.py
+++ 7k-dataset-api.py
@@ -5,6 +5,8 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#hi nauman please help
+
 app = Flask(__name__)
 CORS(app)
 
