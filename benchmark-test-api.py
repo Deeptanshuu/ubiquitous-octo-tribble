a12720d75041d037714955807d731b7c64451d10
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2024-08-27 11:22:36 +0530

    benchmark.py

--- benchmark-test-api.py
+++ benchmark-test-api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 url = "http://localhost:5000/api/recommend"
 data = {
@@ -9,26 +10,36 @@ data = {
     "veg": False
 }
 
+total_og_time = 0
+total_bf_time = 0
 
-response = requests.post(url, json=data)
-results = response.json()
+for _ in range(10):
+    start_time = time.time()
+    response = requests.post(url, json=data)
+    end_time = time.time()
+    total_og_time += (end_time - start_time) * 100
 
-# Print execution times
-og_time = results['original']['execution_time']*100
-bf_time = results['brute_force']['execution_time']*100
+    start_time = time.time()
+    response = requests.post(url, json={**data, "brute_force": True})
+    end_time = time.time()
+    total_bf_time += (end_time - start_time) * 100
 
-print(f"Original approach time: {og_time:.3f} ms")
-print(f"Brute force approach time: {bf_time:.3f} ms")
+average_og_time = total_og_time / 10
+average_bf_time = total_bf_time / 10
 
-print(f"Speed Factor: {(bf_time/og_time):.2f}x times FASATER than brute force approach")
+print(f"Average original approach time: {average_og_time:.3f} ms")
+print(f"Average brute force approach time: {average_bf_time:.3f} ms")
+
+print(f"Speed Factor: {(average_bf_time/average_og_time):.2f}x times FASATER than brute force approach")
 
 print(data)
 # Print recommendations from the original approach
 print("\nTF-IDF vector Approach Recommendations:")
-for recipe in results['original']['recommendations']:
+for recipe in response.json()['original']['recommendations']:
     print(f"ID: {recipe['id']}, Title: {recipe['title']}")
 
 # Print recommendations from the brute force approach
 print("\nBrute Force Approach Recommendations:")
-for recipe in results['brute_force']['recommendations']:
+for recipe in response.json()['brute_force']['recommendations']:
     print(f"ID: {recipe['id']}, Title: {recipe['title']}")
+
