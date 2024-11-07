7af498f663413a9d1c38a5199296ea6a37f6b248
Deeptanshu Lal <laldeeptanshu96@gmail.com>
2024-11-07 22:42:16 +0530

    lil footer change

--- Prod/yes-chef/src/components/Footer.jsx
+++ Prod/yes-chef/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="bg-black text-white py-10 mt-0">
       <div className="container mx-auto text-center">
         <p className="text-m">
-          Made by <strong>Deeptanshu Lal</strong> | Mini Project 2024
+          Made by <strong>Deeptanshu Lal, Nauman Pathan, Sara Pathan, Soham Pardhi</strong> | Mini Project 2024
         </p>
       </div>
     </footer>
